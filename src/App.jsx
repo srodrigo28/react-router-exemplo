1461112c3983edc08f5620e56d4682a8e5d26d94
Sebastião Rodrigo <rodrigoexer1@gmail.com>
2023-05-29 11:49:46 -0300

    create-navegation

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router-dom'
+import Navbar from './components/NavBar'
 
 function App() {
 
   return (
     <>
+      <Navbar />
       <Outlet />
     </>
   )
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const NavBar = () => {
     return (
-        <div>
-
-        </div>
+        <nav className='menu-bar'>
+            <ul>
+                <li> <Link to="/" >Home</Link></li>
+                <li> <Link to="/contact">Contact</Link></li>
+            </ul>
+        </nav>
     )
 }
 
