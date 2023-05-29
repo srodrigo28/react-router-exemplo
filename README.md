1958689f0e4f0156b446892c45683605b439304e
Sebastião Rodrigo <rodrigoexer1@gmail.com>
2023-05-29 11:41:09 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+### React Context & Router v6
+    
+* Referências
+```
+https://www.youtube.com/watch?v=OLtpJLQLOeM&t=825s
+```
+
+* Doc React Router
+```
+https://reactrouter.com/en/main/start/overview
+```
---
+++ src/main.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+
+import App from './App'
+import Home from './routes/Home'
+import Contact from './routes/Contact'
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/contact", element: <Contact /> },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+      <RouterProvider router={router} />
+  </React.StrictMode>
+);
